5edcf05e356d438cc7d6bf726e2af7a5a9919a41
Minh Nhat Hoang <nhathm.uet@gmail.com>
2022-05-21 19:17:42 +0700

    update

--- server/app/Http/Controllers/ProfileController.php
+++ server/app/Http/Controllers/ProfileController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ProfileRequest;
 use App\Models\Profile;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function getAll() {
         return Profile::all();
     }
+
+    public function index() {}
 
     public function findByUsername($username) {
         return User::where('username', $username)->with('profile')->get();
@@ -20,9 +23,10 @@ class ProfileController extends Controller
         return User::where('username', 'LIKE', '%' . $username . '%')->with('profile')->get();
     }
 
-    public function update(ProfileRequest $request)
+    public function update(Request $request)
     {
-        $profile = auth()->user()->profile->update(array_filter($request->all()));
+        return $request;
+        $profile = auth()->user()->profile->update();
         return $profile;
     }
 }
