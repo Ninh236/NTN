e1d9f0e72c579ab4c90a068e1fae722c07318f8f
PhongVan <fmlphongvan@gmail.com>
2022-05-22 00:21:08 +0700

    Add user post

---
+++ client/src/components/Post/UserPost/UserPost.tsx
@@ -0,0 +1,77 @@
+import { Avatar, Box, Button, ButtonGroup, Container, Grid, IconButton, Typography } from "@mui/material";
+import { ReactElement, useState } from "react";
+import PostAction from "../PostAction/PostAction";
+import { useStyle } from "./UserPostStyle";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import CommentOutlinedIcon from "@mui/icons-material/CommentOutlined";
+import Comment from "../Comment/Comment";
+
+interface UserPostProps {
+	postId: number;
+	userId: number;
+}
+
+export default function UserPost(props: any): ReactElement {
+	const styles = useStyle();
+
+	const [userData, setUserData] = useState({
+		id: 0,
+		fullName: "",
+		username: "",
+	});
+	
+	const [showComment, setShowComment] = useState<boolean>(false);
+
+	const handleClickComments = () => {
+		setShowComment(!showComment);
+	};
+
+	return (
+		<Box className={styles.root}>
+			<Container>
+				<Grid container>
+					<Grid item xs={1}><Avatar sx={{ width: 44, height: 44, mx: "auto" }} /></Grid>
+					<Grid item xs={10}>
+						<Typography variant="body1">
+							{userData.fullName} <Typography component="span" variant="caption">{`@${userData.username}`}</Typography>
+						</Typography>
+						<Typography variant="subtitle2"> 3 phút trước</Typography>
+					</Grid>
+					<Grid item xs={1}>
+						<PostAction />
+					</Grid>
+					<Grid item xs={12} my={2} px={2}>
+						asdasdasddasd
+						asdasdasddasdadsa
+						sda
+						sdada
+						sd
+						adaasd
+						asdasdasddasdadsaasd
+						as
+						asdsssssssssssssssssssssssssssssssssssss
+						asd
+					</Grid>
+					<Grid item xs={12} justifyContent="space-between" display="flex">
+						<Typography component="span" variant="subtitle2">12 người thích</Typography>
+						<Typography component="span" variant="subtitle2">5 bình luận</Typography>
+					</Grid>
+					<Grid item xs={12} justifyContent="space-between">
+						<ButtonGroup variant="text" fullWidth>
+							<Button startIcon={<FavoriteBorderIcon />}>
+								Thích
+							</Button>
+							<Button startIcon={<CommentOutlinedIcon />} color="secondary" onClick={handleClickComments}>
+								Bình luận
+							</Button>
+						</ButtonGroup>
+					</Grid>
+					<Grid item xs={12} display={showComment ? "block" : "none"}>
+						<Comment />
+						<Comment />
+					</Grid>
+				</Grid>
+			</Container>
+		</Box>
+	);
+}
---
+++ client/src/components/Post/UserPost/UserPostStyle.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from "@mui/styles";
+import bgImg from "../../assets/imgs/bg-01.webp";
+
+export const useStyle = makeStyles({
+	root: {
+		maxWidth: "40rem",
+		margin: "auto",
+		padding: "1rem",
+		paddingBottom: 0,
+		backgroundColor: "white",
+	}
+});
